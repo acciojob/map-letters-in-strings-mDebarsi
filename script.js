3873bced1d6bd6bdbb17525bde7ff34cc0d17f08
DEBARSI MONDAL <141905446+mDebarsi@users.noreply.github.com>
2023-12-15 19:49:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your JS code here. If required.
+function mapLetters(word) {
+  const letterMap = {};
+
+  // Iterate through each letter in the word
+  for (let i = 0; i < word.length; i++) {
+    const letter = word[i];
+
+    // If the letter is not a key in the letterMap, create an array
+    // Otherwise, push the index to the existing array
+    if (!(letter in letterMap)) {
+      letterMap[letter] = [i];
+    } else {
+      letterMap[letter].push(i);
+    }
+  }
+
+  return letterMap;
+}
